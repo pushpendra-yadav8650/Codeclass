1524af20d5050b01e868a9d799a3c5785efc52ed
pushpendra <pushpedrayadav8434@gmail.com>
2023-12-29 13:04:09 +0530

    add new files

---
+++ course.php
@@ -0,0 +1,59 @@
+<!-----------------------------header----------------------->
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Coding With Pushpendra</title>
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/style.css">
+     <!---- font-awesome------------->
+    <link rel="stylesheet" href="css/all.min.css">
+    <!---- font-awesome------------->
+    <!---------------------goggle font------------>
+    <link href="https://fonts.googleapis.com/css2?family=Ubuntu:ital,wght@1,500&display=swap" rel="stylesheet">
+    <!---------------------google font------------>
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.0/css/all.css" integrity="sha384-lZN37f5QGtY3VHgisS14W3ExzMWZxybE1SJSEsQp9S+oqd12jhcu+A56Ebc1zFSJ" crossorigin="anonymous">
+</head>
+<body>
+<?php
+ include "header.html"
+?>
+<!---------------------header------end----------------------------->
+<div class="container-fluid">
+    <div class="row">
+        <div class="col-md-6 mt-5">
+            <h1 class="heading1">better solution for  your passoin</h1>
+            <p class="pg-1">Are you a student looking for assistance with your college projects? We are here to help! Our team specializes in providing tailored solutions to help you excel in your academic endeavors. Whether it's developing a software application, building a hardware prototype, or conducting research, we've got you covered. We are team of talented designers making project as per your need</p>
+        </div>
+        <div class="col-md-6 image">
+            <img src="image/bg-1.png" width="100%" height="100%">
+        </div>
+    </div>
+</div>
+<div class="container-fluid">
+    <div class="row"></div>
+    <div class="row bg-light">
+    <div class="col-md-12 text-center mt-3 mb-3 pg-3"><h2> Explore Our Courses <img src=""></h2>
+<p class="text-center pg-3">At Codetroops, we offer a wide range of courses designed to help you master in-demand skills and advance your career. Our expert instructors provide comprehensive guidance and hands-on experience in the following areas:</p></div>
+    </div>
+</div>
+<div class="container-fluid">
+   <div class="row">
+    <div class="col-md-4">
+        <div class="card">
+            <div class="card-body">
+                
+            </div>
+        </div>
+    </div>
+    <div class="col-md-4"></div>
+    <div class="col-md-4"></div>
+   </div>
+</div>
+<!-----------------------Footer----------------->
+<?php 
+include "footer.html"
+?>
+<!-----------------------Footer--end----------------->
+</body></html>
---
+++ index.php
@@ -0,0 +1,370 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Coding With Pushpendra</title>
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/style.css">
+     <!---- font-awesome------------->
+    <link rel="stylesheet" href="css/all.min.css">
+    <!---- font-awesome------------->
+    <!---------------------goggle font------------>
+    <link href="https://fonts.googleapis.com/css2?family=Ubuntu:ital,wght@1,500&display=swap" rel="stylesheet">
+    <!---------------------google font------------>
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.0/css/all.css" integrity="sha384-lZN37f5QGtY3VHgisS14W3ExzMWZxybE1SJSEsQp9S+oqd12jhcu+A56Ebc1zFSJ" crossorigin="anonymous">
+</head>
+<body>
+    <?php include "header.html"
+    ?>
+<!-- <div class="container-fluid sticky-top">
+    <div class="row bg-darkblue">
+    <div class="col-md-4 logo col-sm-6 col-6">{CWP}</div>
+    <div class="col-md-8 col-sm-6 col-6">
+        <div class="drop" onclick="abcd()">&#9776;</div>
+        <nav id="navs">
+            <ul class="nav justify-content-end my-nav">
+            <li class=""><a href="/" class="nav-link">Home</a></li>
+            <li class=""><a href="/" class="nav-link">Course</a></li>
+            <li class=""><a href="/" class="nav-link">About</a></li>
+            <li class=""><a href="/" class="nav-link">Project</a></li>
+            <li class=""><a href="/" class="nav-link">Contact</a></li>
+            </ul>
+        </nav>
+        
+    </div>
+</div>
+</div> -->
+
+<div class="container-fluid">
+    <div class="row">
+        <div class="col-md-6 mt-5">
+            <h1 class="heading1">better solution for  your passoin</h1>
+            <p class="pg-1">Are you a student looking for assistance with your college projects? We are here to help! Our team specializes in providing tailored solutions to help you excel in your academic endeavors. Whether it's developing a software application, building a hardware prototype, or conducting research, we've got you covered. We are team of talented designers making project as per your need</p>
+        </div>
+        <div class="col-md-6 image">
+            <img src="image/bg-1.png" width="100%" height="100%">
+        </div>
+    </div>
+</div>
+<div class="container-fluid bg-light">
+    <div class="row">
+        <div class="col-md-12 text-center mt-5"><h2>About us <img src=""></h2></div>
+    </div>
+    <div class="row">
+        <div class="col-md-6">
+            <img src="image/bg-2.png"  width="100%" height="100%">
+        </div>
+        <div class="col-md-6">
+            <p class="pg-2">Coding With Pushpendra,is a leading website design and Project company, comprising a team of highly qualified professionals. Our expertise lies in understanding consumer behaviors and optimizing user experience to create tailored websites that align with purchase behaviors. We take immense pride in our meticulous approach, following a proprietary 162-step procedure to deliver websites of exceptional quality, completely error-free. By adhering to this rigorous process, we consistently generate outstanding results for our clients. In addition to our website design and Project services, Coding With Pushpendra specializes in Android and iOS app Project. Our skilled team of developers stays up to date with the latest technologies and trends to create engaging and functional mobile applications.</p>
+        </div>
+    </div>
+</div>
+
+<div class="container-fluid mb-5">
+    <div class="row">
+        <div class="col-md-6 mt-5">
+             <section class="course">
+                <div class="heading">Our Courses</div>
+                <div class="sub mt">
+                    <div class="top" onclick="abc(0)">Web Designing</div>
+                    <div class="content show">
+                        <p>This course is a very esau design by Codung with pushependra company and provied a very eays contentThis course is a very esau design by Codung with pushependra company and provied a very eays contentThis course is a very esau design by Codung with pushependra company and provied a very eays contentThis course is a very esau design by Codung with pushependra company and provied a very eays content</p>
+                    </div>
+                </div>
+                <div class="sub mt">
+                    <div class="top" onclick="abc(1)">App Devlopment</div>
+                    <div class="content ">
+                        <p>This course is a very esau design by Codung with pushependra company and provied a very eays contentThis course is a very esau design by Codung with pushependra company and provied a very eays contentThis course is a very esau design by Codung with pushependra company and provied a very eays contentThis course is a very esau design by Codung with pushependra company and provied a very eays content</p>
+                    </div>
+                </div>
+                <div class="sub mt">
+                    <div class="top" onclick="abc(2)">Internet of Thing</div>
+                    <div class="content ">
+                        <p>This course is a very esau design by Codung with pushependra company and provied a very eays contentThis course is a very esau design by Codung with pushependra company and provied a very eays contentThis course is a very esau design by Codung with pushependra company and provied a very eays contentThis course is a very esau design by Codung with pushependra company and provied a very eays content</p>
+                    </div>
+                </div>
+                <div class="sub mt">
+                    <div class="top" onclick="abc(3)">Digital Marketing </div>
+                    <div class="content ">
+                        <p>This course is a very esau design by Codung with pushependra company and provied a very eays contentThis course is a very esau design by Codung with pushependra company and provied a very eays contentThis course is a very esau design by Codung with pushependra company and provied a very eays contentThis course is a very esau design by Codung with pushependra company and provied a very eays content</p>
+                    </div>
+                </div>
+                <!-- <div class="sub mt">
+                    <div class="top" onclick="abc(4)">SEO</div>
+                    <div class="content ">
+                        <p>This course is a very esau design by Codung with pushependra company and provied a very eays contentThis course is a very esau design by Codung with pushependra company and provied a very eays contentThis course is a very esau design by Codung with pushependra company and provied a very eays contentThis course is a very esau design by Codung with pushependra company and provied a very eays content</p>
+                    </div>
+                </div>
+                <div class="sub mt">
+                    <div class="top" onclick="abc(5)">Project</div>
+                    <div class="content ">
+                        <p>This course is a very esau design by Codung with pushependra company and provied a very eays contentThis course is a very esau design by Codung with pushependra company and provied a very eays contentThis course is a very esau design by Codung with pushependra company and provied a very eays contentThis course is a very esau design by Codung with pushependra company and provied a very eays content</p>
+                    </div>
+                </div> -->
+             </section>
+        </div>
+        <div class="col-md-6 img-fluid">
+            <img src="image/bg-1.png" width="100%" height="100%">
+        </div>
+    </div>
+</div><br>
+<div class="container-fluid bg-light">
+    <div class="row">
+        <div class="col-md-12 text-center mt-4"><h2 class="pg-3"><b>OUR SERVICES</b></h2>
+        <p class="text-center">At Codetroops, we provide a range of expert digital solutions to help your business thrive. Our dedicated team delivers high-quality services tailored to your specific needs, ensuring exceptional results.</p>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-md-3">
+            <div class="card mb-4">
+                <div class="card-body">
+                  <img src="">
+                  <i class="fa-solid fa-gears"></i>
+                  <h4 class="text-center pg-3">WEBSITE DESIGNING</h4>
+                  <p class="text-center">We create high-quality professional customized responsive websites for the best price.</p>
+                </div> 
+              </div>
+            </div>
+        <div class="col-md-3">
+            <div class="card mb-4">
+                <div class="card-body">
+                  <img src="">
+                  <i class="fa-solid fa-gears"></i>
+                  <h4 class="text-center pg-3">APP PROJECT</h4>
+                  <p class="text-center">Get your Android or IOS app designed and developed by WebzPlot for Great Results.</p>
+                </div> 
+              </div>
+        </div>
+        <div class="col-md-3">
+            <div class="card mb-4">
+                <div class="card-body">
+                  <img src="">
+                  <i class="fa-solid fa-gears"></i>
+                  <h4 class="text-center pg-3">E-COMMERCE WEBSITE</h4>
+                  <p class="text-center">Sell your products online to your consumers using a professionally built website by WebzPlot.</p>
+              </div>
+        </div>
+    </div>
+    <div class="col-md-3">
+        <div class="card mb-4">
+            <div class="card-body">
+              <img src="">
+              <i class="fa-solid fa-gears"></i>
+              <h4 class="text-center pg-3">IOT Project</h4>
+              <p class="text-center">Sell your products online to your consumers using a professionally built website by WebzPlot.</p>
+          </div>
+    </div>
+</div>
+</div>
+</div>
+<div class="container-fluid mt-5 mb-5">
+    <div class="row">
+        <div class="col-md-9">
+          <h4 class="text-light ml-3">Call To Action</h4>
+          <p class="text-light">Are you ready to take your business to the next level?
+             We're here to help you achieve your goals. Our team of experts will work
+              closely with you to develop customized solutions that drive growth and success.
+             Don't miss out on the opportunity to transform your business. Contact us today</p>
+        </div>
+        <div class="col-md-3">
+            <div class="button"><h5 class="text-light mt-2 ml-3">Call To Action</h5></div>
+        </div>
+    </div>
+</div>
+<div class="container-fluid bg-white">
+    <div class="row">
+        <div class="col-md-12 mt-5">
+            <h3 class="text-center pg-3"><b>OUR TEAM</b> </h3>
+            <p class="text-center pg-3">We are a dedicated team of professionals passionate about technology and innovation. Our mission is to create cutting-edge solutions that solve real-world problems and provide value to our users. With a diverse range of skills and expertise, we collaborate closely to deliver high-quality results.</p>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-md-6 col-sm-5 ">
+            <div class="card">
+                <div class="card-body">
+                    <div class="images"><img src="image/webdev.jpeg" width="100%" height="100%"></div>
+                    <div class="ph"><h4 style="margin-left: 70px;">Anish kumar</h4>
+                    <p style="margin-left: 55px; margin-top: -30px;">(WEB DEVLOPER)</p>
+                    <ul style="margin-left: 90px; margin-top: -15px;">
+                        <a href=""><i class="fab fa-facebook" style="font-size:30px; color:rgb(35, 13, 183);"></a></i>&nbsp;&nbsp;&nbsp;
+                        <a href=""> <i  class="fab fa-instagram" style="font-size:30px; color:pink;"></a></i>&nbsp;&nbsp;&nbsp;
+                        <a href=""><i class="fab fa-linkedin" style="font-size:30px;"></i></a>
+                     </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-6 col-sm-5 ">
+            <div class="card">
+                <div class="card-body">
+                <div class="images"><img src="image/appdev.jpeg" width="100%" height="100%"></div>
+                 <div class="ph"><h4 class="ml-4">Pushpendra yadav</h4>
+                 <p style="margin-left: 55px; margin-top: -30px;">(APP DEVLOPMENT)</p>
+                 <ul style="margin-left: 90px; margin-top: -15px;">
+                    <a href=""><i class="fab fa-facebook" style="font-size:30px; color:rgb(25, 17, 176);"></a></i>&nbsp;&nbsp;&nbsp;
+                    <a href=""> <i  class="fab fa-instagram" style="font-size:30px; color:pink;"></a></i>&nbsp;&nbsp;&nbsp;
+                    <a href=""><i class="fab fa-linkedin" style="font-size:30px;"></i></a>
+                 </ul>
+                 </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<!-- <div class="conatiner-fluid bg-light">
+    <div class="row mt-5">
+        <div class="col-md-12">
+            <h3 class="text-center pg-3 mt-5"><b>CONTACT US</b> </h3>
+            <p class="text-center pg-3">We are a dedicated team of professionals passionate about technology and innovation. Our mission is to create cutting-edge solutions that solve real-world problems and provide value to our users. With a diverse range of skills and expertise, we collaborate closely to deliver high-quality results.</p>
+        </div>
+        </div>
+   
+<div class="row">
+    <div class="col-md-6"></div>
+     <div class="col-md-6">
+      
+     </div>
+</div>
+</div> -->
+<div class="container-fluid bg-light">
+    <div class="row">
+    <div class="col-md-12">
+        <h3 class="text-center pg-3"><b>Contact</b> </h3>
+        <p class="text-center pg-3">Contact us today to discuss your project requirements and take your college projects to the next level.</p>
+    </div>
+    </div>
+    <div class="row">
+        <div class="col-md-5">
+            <h4 class="hg ml-3">Location:</h4>
+                <p class="pg-3">Shikhobad (Firozabad)<br>
+                Utter Pradesh (283141)<br> 
+               <h4 class="hg ml-3">Email:-</h4><p><a href="mailto:"> pushpendrayadav8434@gmail.com</a></p>
+               <h4 class="hg ml-3">Phone:-</h4> <p>+918588978545</p>
+
+        </div>
+        <div class="col-md-7 frm mb-4">
+            <form>
+                <div class="row mt-3">
+                    <div class="col">
+                        <label for="email" class="mb-2 mr-sm-2"> <b>Your Name</b></label>
+                        <input type="text" class="form-control" id="name" placeholder="Enter Your Name" name="name">
+                    </div>
+                    <div class="col">
+                        <label for="email" class="mb-2 mr-sm-2"><b> Your Email</b></label>
+                        <input type="text" class="form-control" id="name" placeholder="Enter Your Name" name="name">
+                    </div>
+                    
+                </div><br>
+                <div class="form-group">
+                    <label for="email"><b> Subject</b></label>
+                    <input type="email" class="form-control" id="email" placeholder="Enter email" name="email">
+                </div>
+                <div class="form-group">
+                    <label for="comment"><b>Comment</b></label>
+                    <textarea class="form-control" rows="7" id="comment"></textarea>
+                </div>
+                <button class="btn btn-info btnn">Send Message</button>
+            </form>
+        </div>
+    </div>
+</div>
+<div class="container-fluid bg-white ">
+    <div class="row">
+        <div class="col-md-12  mt-4 mb-4">
+
+            <h3 class="text-center pg-3"><b>Join Our Coding Family</b> </h3>
+        <p class="text-center pg-3">To stay updated on our latest posts and courses, we invite you to connect with us through the following links</p>
+    
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-md-3 col-sm-6  ty">
+            <img src="Discord_Oct_Denoiser_Beauty_001_copy_a253ed4d28.webp">
+        </div>
+        <div class="col-md-3 col-sm-6 ty">
+            <img src="Linked_In_Oct_Denoiser_Beauty_001_copy_b6e0b974bb.webp">
+        </div>
+        <div class="col-md-3 col-sm-6  ty">
+            <img src="Youtube_Oct_Denoiser_Beauty_001_copy_6a63d8ef65.webp">
+        </div>
+        <div class="col-md-3 col-sm-6 ">
+            <img src="Telegram_Oct_Denoiser_Beauty_002_copy_fa9aeac119.webp">
+        </div>
+    </div>
+</div>
+<!-----------------------------------------footeer------------------------->
+    <div class="container-fluid bg-lights mt-4">
+        <div class="row">
+            <div class="col-md-3  mt-5 text-warning ">
+                <h4 class="hg">CodingWithPushendra</h4>
+                <p class="pg-3">Shikhobad (Firozabad)<br>
+                Utter Pradesh (283141)<br> 
+                <b>Phone:-</b> +918588978545<br>
+               <b>Email:-</b><a href="mailto:"> pushpendrayadav8434@gmail.com</a></p>
+            </div>
+            <div class="col-md-3  mt-5">
+                <h6 class="hg">UseFul Links</h6>
+                <ul class="pg-3">
+                    <li>Home</li>
+                    <li>About us</li>
+                    <li>Services</li>
+                    <li>Term of Services</li>
+                    <li>privacy policy</li>
+                </ul>
+            </div>
+            <div class="col-md-3 mt-5">
+                <h6 class="hg">Our services</h6>
+                <ul class="pg-3">
+                    <li>Web Project</li>
+                    <li>Full Stack Web devlopment</li>
+                    <li>E-Commerce Project</li>
+                    <li>IOT Project</li>
+                    <li>ios project</li>
+                    <li>Android App project</li>
+                </ul>
+            </div>
+            <div class="col-md-3 mt-5">
+                <h6 class="hg">Our Social Network</h6>
+                <p class="pg-3">Fell free To Connect Us by Social Network</p>
+                <i class=""></i>
+                <i class=""></i>
+                <i class=""></i>
+            </div>
+        </div>
+    </div>
+<!-----------------------------------------footeer------------------------->
+<div class="container-fluid">
+    <div class="row text-light">
+        <div class="col-md-6">
+            <footer>
+                <p>Copyright © 2023 CodingWithPushendra. All Rights Reserved</p>
+            </footer>
+        </div>
+        <div class="col-md-6  ftr">
+            <footer>
+                <p style="margin-left: 60px;">A unit of <a href="">CodingWithPushendra pvt Ltd</a></p>
+            </footer>
+        </div>
+    </div>
+</div>
+
+
+</body>
+</html>
+<script>
+    function abcd() {
+		document.getElementById("navs").classList.toggle("show");
+	}
+
+    function abc(n){
+        var data = document.getElementsByClassName("content");
+        for(i=0;i<data.length;i++)
+        {
+            data[i].style.height="0";
+            data[n].style.height="180px";
+
+        }
+
+    }
+</script>
